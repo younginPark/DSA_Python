51b5a60bad75c5b19471b80ee8b16611a0159436
younginPark <youngin.park9@gmai.com>
2023-08-13 19:01:15 +0900

    [ADD] Linked List append, pop, prepend

--- CodingExercise/LinkedLists_node.py
+++ CodingExercise/LinkedLists_node.py
@@ -8,13 +8,59 @@ class LinkedList:
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-        self.legnth = 1
+        self.length = 1 
 
-def print_list(self):
-    temp = self.head
-    while temp is not None:
-        print(temp.value)
-        temp = temp.next
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
 
-my_linked_list = LinkedList(4)
-print(my_linked_list.head.value)
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
+
+# my_linked_list.print_list()
+
+# print(my_linked_list.pop())
+# print(my_linked_list.pop())
+# print(my_linked_list.pop())
+
+my_linked_list.prepend(1)
+my_linked_list.print_list()
